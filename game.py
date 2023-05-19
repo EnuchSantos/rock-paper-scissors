5c7b1acd68488c6551b37e3959793802380f5df8
EnuchSantos <enuchdev@gmail.com>
2023-05-19 09:44:25 -0300

    refactoring complete

--- game.py
+++ game.py
@@ -1,26 +1,37 @@
 import random
 
 def game_run():
-    user = input(f"What's your choice? 'r' for ✊ 'p' for 🖐 's' for 🖖\n")
-    computer = random.choice(['r', 'p', 's'])
+    user_points = 0
+    computer_points = 0
+    for hit in range(3):
+        user = input(f"What's your choice? 'r' for ✊ 'p' for 🖐 's' for 🖖\n")
+        computer = random.choice(['r', 'p', 's'])
+        
+        status = rules(user, computer)
+        
+        if status == 1:
+            user_points += 1
+        elif status == 2:   
+            computer_points += 1 
+        print(f'user:{user_points} computer: {computer_points}\n{user} x {computer}') 
     
-    status = rules(user, computer)
-    
-    if status == 1:
-        print(f'You won! Congratulations!\n {user} x {computer}')
-    elif status == 2:
-        print(f'Draw!!\n {user} x {computer}')
+    if user_points > computer_points:
+        print(f'You won! Congratulations!')
+    elif computer_points > user_points:
+        print(f'You lost!!')
     else:
-        print(f'You lost!!\n {user} x {computer}')
+        print(f'Draw!!') 
+                   
 
 def rules(user: str, computer: str) -> int:
     if (user == 'r' and computer == 's') or (user == 'p' and computer == 'r') \
         or (user == 's' and computer == 'p'):
         return 1
-    elif (user == 'r' and computer == 'r') or (user == 'p' and computer == 'p') \
-        or (user == 's' and computer == 's'):
+    elif (user == 's' and computer == 'r') or (user == 'r' and computer == 'p') \
+        or (user == 'p' and computer == 's'):
         return 2
     
     return 3
-
+    
+    
 game_run()
