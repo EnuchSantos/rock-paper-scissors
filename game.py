ef7d2d26b56a89ae487708d0c97044b6b567122c
EnuchSantos <enuchdev@gmail.com>
2023-05-19 09:11:45 -0300

    refactoring the game hangman with functions

--- game.py
+++ game.py
@@ -1,14 +1,26 @@
 import random
 
-while True:
+def game_run():
     user = input(f"What's your choice? 'r' for ✊ 'p' for 🖐 's' for 🖖\n")
     computer = random.choice(['r', 'p', 's'])
+    
+    status = rules(user, computer)
+    
+    if status == 1:
+        print(f'You won! Congratulations!\n {user} x {computer}')
+    elif status == 2:
+        print(f'Draw!!\n {user} x {computer}')
+    else:
+        print(f'You lost!!\n {user} x {computer}')
 
+def rules(user: str, computer: str) -> int:
     if (user == 'r' and computer == 's') or (user == 'p' and computer == 'r') \
         or (user == 's' and computer == 'p'):
-        print(f"You won!\nyou: {user}\ncomputer: {computer}")
-        continue
+        return 1
+    elif (user == 'r' and computer == 'r') or (user == 'p' and computer == 'p') \
+        or (user == 's' and computer == 's'):
+        return 2
+    
+    return 3
 
-    print(f"You lost!\nyou: {user}\ncomputer: {computer}")
-    continue
-
+game_run()
